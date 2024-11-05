0a354f69e860f1bea6e65f305ea9571269c2aa44
bapricethompson <123414216+bapricethompson@users.noreply.github.com>
2024-11-05 11:30:39 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,18 +1,88 @@
 let artifactLst=document.querySelector("#artifactList");
+const baseUrl="https://artifacts-api.up.railway.app";
+
+let submitButton=document.querySelector("#commentSubmit");
+let commentList=document.querySelector("#commentList");
+
+let currentArtifactId=null;
+
 
 const closeButton = document.getElementsByClassName("close")[0];
 closeButton.onclick = function() {
     const modal = document.getElementById("myModal");
-    modal.style.display = "none"; // Hide the modal
+    modal.style.display = "none"; 
 };
 
-// Close modal when clicking outside of it
 window.onclick = function(event) {
     const modal = document.getElementById("myModal");
     if (event.target === modal) {
-        modal.style.display = "none"; // Hide the modal
+        modal.style.display = "none"; 
     }
 };
+
+function loadComments(artifactId){
+    commentList.innerHTML = "";
+    fetch(`${baseUrl}/artifacts/${artifactId}/comments`).then(function (response){
+        response.json().then(function (data){
+            console.log("mas",data);
+            console.log(data[0]);
+            for (let com of data){
+                newLi=document.createElement("li");
+                newLi.textContent=com.text;
+                
+                let details = document.createElement("ul");
+                let detailsLi = document.createElement("li");
+                detailsLi.style.listStyleType = "none"; 
+                detailsLi.textContent = "by " + com.name + " on " + new Date(com.date).toLocaleDateString(); 
+                details.appendChild(detailsLi);
+
+                newLi.appendChild(details);
+                commentList.appendChild(newLi); 
+            }
+        })
+    });
+}
+
+function createComment(artifactId, comment, name){
+    let data = new FormData();
+    data.append("name",name);
+    data.append("text",comment);
+    fetch(`${baseUrl}/artifacts/${artifactId}/comments`, {
+        method: "POST",
+        body:data
+    }).then(function (response){
+        loadComments(artifactId);    
+    });
+
+}
+
+submitButton.addEventListener("click", function(){
+    let commentText=document.querySelector("#commentText");
+    let commentName=document.querySelector("#commentName");
+
+    commentText.style.backgroundColor = "white";
+    commentName.style.backgroundColor = "white";
+
+    if (commentText.value.trim() === "" || commentName.value.trim() === "") {
+        //CHECK TO MAKE SURE DATA ISN" EMPTY AND USE CSS TO SHOW ERRORS
+        if (commentText.value.trim()==="" && commentName.value.trim()===""){
+            commentText.style.backgroundColor ="#f09292";
+            commentName.style.backgroundColor ="#f09292";
+        }
+        else if (commentText.value.trim()===""){
+            commentText.style.backgroundColor ="#f09292";
+        }
+        else if (commentName.value.trim()===""){
+            commentName.style.backgroundColor ="#f09292";
+        }
+      } else {
+        createComment(currentArtifactId, commentText.value, commentName.value);
+        commentText.value="";
+        commentName.value="";
+      }
+})
+
+
 
 
 // creates async request to web API
@@ -106,6 +176,9 @@ fetch("https://artifacts-api.up.railway.app/artifacts").then(function (response)
                 detailsDiv.appendChild(detailsList);
                 modalBigDiv.appendChild(detailsDiv);
 
+
+                currentArtifactId = artifact.accession_number;
+                loadComments(currentArtifactId);
                 modal.style.display = "block";
                 
                 
